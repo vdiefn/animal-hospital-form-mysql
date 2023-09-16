a696624d7d49cf4e602c51341f29cf61d4ce0033
vdiefn <ddhkxxhxx@hotmail.com>
2023-09-16 19:12:44 +0800

    feat:add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
 const hospitalController = require('../controllers/hospital-controller')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 
 router.get('/hospitals', hospitalController.getHospitals)
 
---
+++ routes/modules/admin.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const adminController = require('../../controllers/admin-controller')
+
+
+router.get('/hospitals', adminController.getHospitals)
+
+router.use('/', (req, res) => res.redirect('/admin/hospitals'))
+
+module.exports = router
