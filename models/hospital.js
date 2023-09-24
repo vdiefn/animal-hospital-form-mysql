8d9eaa6f418b4ec57a4d716d3c2292ec443e2bdb
vdiefn <ddhkxxhxx@hotmail.com>
2023-09-24 14:18:36 +0800

    feat:add restaurant model

---
+++ models/hospital.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Hospital extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Hospital.init({
+    name: DataTypes.STRING,
+    city: DataTypes.STRING,
+    tel: DataTypes.STRING,
+    address: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    image: DataTypes.STRING,
+    openingHours: DataTypes.STRING,
+    closingHours: DataTypes.STRING,
+    website: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Hospital',
+    tableName: 'Hospitals',
+    underscored: true,
+  });
+  return Hospital;
+};
