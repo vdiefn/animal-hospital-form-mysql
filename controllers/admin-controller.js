6bd79cf9b883b30b99db8195f1ef2e47fadb3d42
vdiefn <ddhkxxhxx@hotmail.com>
2023-10-03 16:15:38 +0800

    feat: admin update hospital

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -17,7 +17,6 @@ const adminController = {
   },
   postHospital: (req, res, next) => {
     const { name, city, tel, address, description, image, openingHours, closingHours, website } = req.body
-    console.log(req.body)
     if (!name ) throw new Error('請填上醫院名稱！')
     if (!city) throw new Error('請填上醫院所在縣市！')
     if (!tel) throw new Error('請填上醫院電話號碼！')
@@ -49,7 +48,43 @@ const adminController = {
       res.render('admin/hospital', { hospital })
     })
     .catch(err => next(err))
-  }
+  },
+  editHospital:(req, res, next) => {
+    Hospital.findByPk(req.params.id, {
+      raw: true
+    })
+    .then(hospital => {
+      if (!hospital) throw new Error ('該醫院不存在！')
+      res.render('admin/edit-hospital', { hospital })
+    })
+    .catch(err => next(err))
+  },
+  putHospital: (req, res, next) => {
+    const { name, city, tel, address, description, image, openingHours, closingHours, website } = req.body
+    if (!name) throw new Error('請填上醫院名稱！')
+    if (!city) throw new Error('請填上醫院所在縣市！')
+    if (!tel) throw new Error('請填上醫院電話號碼！')
+    if (!openingHours || !closingHours) throw new Error('請填上醫院營業時間！')
+    if (!description) throw new Error('請填上醫院簡介！')
+    Hospital.findByPk(req.params.id )
+      .then(hospital => {
+        if(!hospital) throw new error('該醫院不存在！')
+        return hospital.update({
+          name,
+          city,
+          address,
+          description,
+          openingHours,
+          closingHours,
+          website
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '已成功修改醫院資訊！')
+        res.redirect('/admin/hospitals')
+      })
+      .catch(err => next(err))
+    } 
 }
 
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,9 +4,13 @@ const adminController = require('../../controllers/admin-controller')
 
 
 router.get('/hospitals/create', adminController.createHospital)
-router.post('/hospitals', adminController.postHospital)
+
+router.get('/hospitals/:id/edit', adminController.editHospital)
+router.put('/hospitals/:id', adminController.putHospital)
 
 router.get('/hospitals/:id', adminController.getHospital)
+router.post('/hospitals', adminController.postHospital)
+
 
 router.get('/hospitals', adminController.getHospitals)
 
