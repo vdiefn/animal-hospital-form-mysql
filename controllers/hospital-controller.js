3fe16e37204699d16357fd5268522da100fb3e40
vdiefn <ddhkxxhxx@hotmail.com>
2023-10-20 15:33:56 +0800

    feat: add hospitals index page

--- controllers/hospital-controller.js
+++ controllers/hospital-controller.js
@@ -1,6 +1,20 @@
+const { Hospital, Location } = require('../models')
+
 const hospitalController = {
   getHospitals: (req, res) => {
-    return res.render('hospitals')
+    return Hospital.findAll({
+      include: Location,
+      raw: true,
+      nest: true
+    })
+    .then(hospitals => {
+      const data = hospitals.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      res.render('hospitals', { hospitals: data })
+    })
+    .catch(err => next(err))
   }
 }
 
